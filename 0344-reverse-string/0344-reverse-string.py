5ee3972cc597694825244d32b6a1e9b29889f59f
NALANAGULA SRAVYA <sravyanalanagula2003@gmail.com>
2023-08-19 16:07:54 +0530

    Time: 196 ms (79.47%), Space: 20.6 MB (86.02%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,8 +4,13 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        
+        """
         i = len(s)
         while(i >= 0):
             s.append(s.pop(i-1))
             i -= 1
+        """
+        n = len(s)
+        for i in range(len(s)//2):
+            s[i],s[n-i-1] = s[n-i-1],s[i]
+        

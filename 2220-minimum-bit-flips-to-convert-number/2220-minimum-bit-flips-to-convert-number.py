7978e8426a16e3e51362b9d069836995988e3715
NALANAGULA SRAVYA <sravyanalanagula2003@gmail.com>
2023-08-19 17:55:11 +0530

    Time: 30 ms (98.06%), Space: 16.3 MB (67.87%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
@@ -1,9 +1,12 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
+        """
         res = start ^ goal
         c = 0
         while res:
             res &= res  - 1
             c += 1
         return c
+        """
+        return (start ^ goal).bit_count()
             

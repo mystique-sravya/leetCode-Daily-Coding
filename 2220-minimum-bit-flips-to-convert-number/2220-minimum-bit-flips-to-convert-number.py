c0fb0594e9bad5803d244f3ea951394c693bcd44
NALANAGULA SRAVYA <sravyanalanagula2003@gmail.com>
2023-08-19 17:49:06 +0530

    Time: 43 ms (57.48%), Space: 16.2 MB (92.24%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        res = start ^ goal
+        c = 0
+        while res:
+            res &= res  - 1
+            c += 1
+        return c
+            

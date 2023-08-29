e41924eee2895ac93a612075f917887d40a2a07d
NALANAGULA SRAVYA <sravyanalanagula2003@gmail.com>
2023-08-29 22:42:35 +0530

    Time: 161 ms (98.20%), Space: 18.2 MB (45.42%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,6 +1,6 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        
+        """
         max_num = float(-inf)
         for n in nums:
             max_num =max(max_num,n)
@@ -16,3 +16,14 @@ class Solution:
                     res[i] = temp[j]
                     break
         return res
+        """
+        n = len(nums)
+        ret = [-1] * n
+        stack = nums[::-1]
+        for i in range(n - 1, -1, -1):
+            while stack and stack[-1] <= nums[i]:
+                stack.pop()
+            if stack:
+                ret[i] = stack[-1]
+            stack.append(nums[i])
+        return ret

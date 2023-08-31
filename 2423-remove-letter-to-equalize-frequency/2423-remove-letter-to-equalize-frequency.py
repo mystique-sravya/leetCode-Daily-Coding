6e0d77281994709dc79fd11ed5555f7f81b49ffb
NALANAGULA SRAVYA <sravyanalanagula2003@gmail.com>
2023-08-31 07:28:30 +0530

    Time: 44 ms (39.78%), Space: 16.2 MB (72.46%) - LeetHub

--- 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.py
+++ 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.py
@@ -1,6 +1,11 @@
 class Solution:
     def equalFrequency(self, word: str) -> bool:
         
+        """
+        (Reduce TC from O(n2) -> O(n)
+
+Instead of checking length of set elements everytime just check when it should be i.e., keep checking the freq of each char by removing 1 from it and also if it is zero.
+        """
         counter = Counter(word)
         for c in word:
             counter[c] -= 1

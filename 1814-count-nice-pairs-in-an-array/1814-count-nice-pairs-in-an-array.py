a545b33ba3a0fad39019e13b2f52ba0d11bec346
NALANAGULA SRAVYA <sravyanalanagula2003@gmail.com>
2023-11-21 18:39:54 +0530

    Time: 622 ms (39.66%), Space: 26.8 MB (61.19%) - LeetHub

---
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countNicePairs(self, nums: List[int]) -> int:
+        res = 0
+        count = collections.Counter()
+        for a in nums:
+            b = int(str(a)[::-1])
+            res += count[a - b]
+            count[a - b] += 1
+        return res % (10**9 + 7)
